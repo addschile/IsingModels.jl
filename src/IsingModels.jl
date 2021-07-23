ad9c8e1dbe0f61f2cf1efcb8179a8402ac2ef8dd
Addison Schile <addschile@gmail.com>
2021-07-22 17:38:00 -0700

    added export statement for use as a pacakge

--- src/IsingModels.jl
+++ src/IsingModels.jl
@@ -2,9 +2,15 @@ module IsingModels
 
 using Random
 
+export IsingModel2D,randomdata,allup,alldown
+export mcsweeps,mcsweep
+export glaubersweeps
+export calc_magnetization,calc_avg_magnetization
+
 include("ising.jl")
 include("callbacks.jl")
 include("mc.jl")
 include("glauber.jl")
+include("observables.jl")
 
 end
